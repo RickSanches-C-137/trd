297d47b2efa9fe6d7317725311b561a2337084c8
Rick C-137 <110402945+RickSanches-C-137@users.noreply.github.com>
2023-08-13 12:44:29 +0100

    Update server.ts db env

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 async function bootstrap() {
     try {
         //
-        await mongoose.connect(process.env.DATABASE_URL!)
+        await mongoose.connect(process.env.DATABASE_URL)
         // console.log('database connected')
 
     } catch (error) {
